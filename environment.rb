d22d555b7f3814eca991ef8e592a9016c98ff599
Billy J Rhoades II <billyarhoades@gmail.com>
2017-02-19 13:05:57 -0600

    Updated environment to use config again.

--- environment.rb
+++ environment.rb
@@ -8,9 +8,9 @@ require 'yaml'
 
 conf = YAML.load_file("config.yml")
 
-#$twitter_client = Twitter::REST::Client.new do |config|
-#  config.consumer_key        = conf["consumer_key"]
-#  config.consumer_secret     = conf["consumer_secret"]
-#  config.access_token        = conf["access_token"]
-#  config.access_token_secret = conf["access_token_secret"]
-#end
+$twitter_client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = conf["consumer_key"]
+  config.consumer_secret     = conf["consumer_secret"]
+  config.access_token        = conf["access_token"]
+  config.access_token_secret = conf["access_token_secret"]
+end
