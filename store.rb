db3b498f6dcacbdf8897a628fd6fef6ed85f5b74
Billy J Rhoades II <billyarhoades@gmail.com>
2017-02-18 23:26:29 -0600

    Updated store script to skip already stored/processed messages.

--- store.rb
+++ store.rb
@@ -1,4 +1,4 @@
-require_relative 'environment'
+require_relative './environment.rb'
 
 # Get the most recent
 # puts $twitter_client.user_timeline("realDonaldTrump")
@@ -16,9 +16,21 @@ end
 tweets.each do |tweet|
   next if tweet["is_retweet"]
 
-  # if Markov::Source.where(text: tweet["text"]).size == 0
-    MarkovSpeech::Storage.store(tweet["text"])
-  # end
+  if Markov::Models::Source.where(text: tweet["text"]).size == 0
+    puts "Adding: \"#{tweet["text"]}\""
+    Markov::Storage::Storage.store(tweet["text"])
+  else
+    puts "Skipping: \"#{tweet["text"]}\""
+  end
+end
 
-  markov_speech.store
+puts "PROCESSING: "
+Markov::Models::Source.all.each do |source|
+  # Assume not processed
+  if Markov::Models::Chain.where(source: source).size == 0
+    Markov::Storage::Storage.process(source)
+    print "."
+  else
+    print "S"
+  end
 end
